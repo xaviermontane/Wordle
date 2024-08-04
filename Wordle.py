d15f76688c2fdd4b03b535a3365c40ca1fd047d4
xaviermontane <xaviermontane3@gmail.com>
2024-08-04 07:06:00 +0200

    Minor changes.

--- Wordle.py
+++ Wordle.py
@@ -3,14 +3,15 @@ from random import choice
 word_list = ["WHILE", "DICTS", "FLOAT", "RANGE", "BREAK", "LOOPS", "INPUT", "PRINT", 
   "TUPLE", "LISTS", "STRIN", "FUNCT", "CLASS", "FILES", "ERROR", "RAISE", 
   "LOCAL", "YIELD", "REGEX", "STRIP"]
-word = choice(word_list)
 
 def play_wordle():
   count = 0
   tries = 6
+  word = choice(word_list)
   hits = ["_", "_", "_", "_", "_"]
 
-  while count < 5 and tries > 0:
+  print("\nWelcome to Wordle! — The five letter word game")
+  while tries > 0:
     print("\n–––––––––––—")
     print(f"You have {tries} tries left.")
     print(f"You have found {count} letters.")
@@ -33,8 +34,19 @@ def play_wordle():
       else:
         print(f"{char} | X")
 
-  if count == 5:
-    print("\nCongratulations, you have solved the Wordle!")
+    if count == 5:
+      print("\nCongratulations, you have solved the Wordle!")
+      print(f"The word was: {word.capitalize()}\n")
+      break
+
+  if tries == 0 and count < 5:
+    print("\nYou have run out of tries!")
     print(f"The word was: {word.capitalize()}\n")
+  
+  play_again = input("Would you like to play again? (y/n): ").lower()
+  if play_again == "y":
+      play_wordle()
+  else:
+    print("\n\nThank you for playing Wordle!\nCredits to Josh Wardle for the original game <3\n")
 
 play_wordle()
